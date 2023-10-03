6bcd5e1b3a891cbccc93f5b46ac8661a31cf3c5f
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-10-03 19:40:43 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var div1=document.getElementById("div1");
+
+div1.style.backgroundColor="Yellow";
+div1.style.margin="20px";
+div1.style.padding="10px";
+div1.style.fontSize="18px";
+div1.style.fontWeight="Bold";
+div1.style.height="200px"
+div1.style.width="300px"
+div1.style.color="blue"
